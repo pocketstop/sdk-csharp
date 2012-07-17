6bb01b693a1bf4deccd8bff3c44d3da046841c53
Adam Anderly <adam@anderly.com>
2012-07-17 14:26:06 -0500

    Added xml-comments to QRCode partial class.

--- src/Pocketstop.Api/QRCode.cs
+++ src/Pocketstop.Api/QRCode.cs
@@ -6,6 +6,12 @@ namespace Pocketstop
 {
 	public partial class PocketstopRestClient
 	{
+		/// <summary>
+		/// Generate a QR Code
+		/// Makes a POST request to the QR resource.
+		/// </summary>
+		/// <param name="data">The data to encode in the QR Code.</param>
+		/// <param name="size">height x width dimensions (default to 150x150 up to max 500x500).</param>
 		public dynamic GenerateQrCode(string data, string size = "150x150")
 		{
 			Require.Argument("data", data);
